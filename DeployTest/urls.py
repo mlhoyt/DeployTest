e840ad4c8a66614899a8454eb7b494630ba20523
Mike Hoyt <derukimlh@gmail.com>
2017-05-26 22:08:11 -0700

    Fixed typo in project urls.py

--- DeployTest/urls.py
+++ DeployTest/urls.py
@@ -16,5 +16,5 @@ Including another URLconf
 from django.conf.urls import url, include
 
 urlpatterns = [
-    url( r'^', url( 'apps.simple.urls', namespace="simple" ) ),
+    url( r'^', include( 'apps.simple.urls', namespace="simple" ) ),
 ]
--- apps/simple/views.py
+++ apps/simple/views.py
@@ -1,4 +1,4 @@
 from django.shortcuts import render, redirect, reverse, HttpResponse
 
 def index( request ):
-    return HttpResponse( "Successfully loaded simple:index")
+    return HttpResponse( "Successfully loaded simple:index" )
